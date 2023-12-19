fd9ba3118509a634304127e81b0016d4bf34e36e
Shusal Lamsal <shusallamsal35@gmail.com>
2023-12-19 15:03:02 +0545

    updated new thind

---
+++ lib/category_list.dart
@@ -0,0 +1,7 @@
+final List<Map<String, String>> childrenContainers = [
+    {"title": "Business", "imageURL": "https://t3.ftcdn.net/jpg/00/97/37/90/500_F_97379028_HD7Gp1cdemIJkYMmVeMqnCIkWUFNXYMf.jpg"},
+    {"title": "Education", "imageURL": "https://th.bing.com/th/id/R.73a10c8f26c5305c8e5b0dcb011ea635?rik=2G7aTi9DvqYjZA&pid=ImgRaw&r=0"},
+    {"title": "Technology", "imageURL": "https://th.bing.com/th/id/R.c59cb4d6dda0d97d72600fc66821bbcd?rik=p%2bqtqo2eNb0jtA&riu=http%3a%2f%2fthestatetimes.com%2fwp-content%2fuploads%2f2018%2f02%2finfotech.jpg&ehk=FF2w7obphoNFJAmHb0IyqQuqdiAVkB1haRtebXosqj8%3d&risl=&pid=ImgRaw&r=0"},
+    {"title": "Health", "imageURL": "https://justiceaction.org.au/wp-content/uploads/2020/09/iStock-639896942.jpg"},
+    {"title": "Science", "imageURL": "https://th.bing.com/th/id/R.f5b157c8033d028c69ff1a25f7d15133?rik=PaThKXiqrYYd5g&riu=http%3a%2f%2fthelibertarianrepublic.com%2fwp-content%2fuploads%2f2016%2f02%2fscience.jpg&ehk=fUOTF2D6E%2fQu6taI9nd58LHfbyA3naqv4va96Cv2mLg%3d&risl=&pid=ImgRaw&r=0"},
+  ];
--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,5 @@
 import 'dart:developer';
+import 'category_list.dart';
 
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
@@ -85,15 +86,15 @@ class _HomePageState extends State<HomePage> {
                   children: [
                     SizedBox(
                       height: 100,
-                      child: ListView(
-                        scrollDirection: Axis.horizontal,
-                        children: [
-                          _buildContainer("Business", "https://t3.ftcdn.net/jpg/00/97/37/90/500_F_97379028_HD7Gp1cdemIJkYMmVeMqnCIkWUFNXYMf.jpg"),
-                          _buildContainer("Education", "https://th.bing.com/th/id/R.73a10c8f26c5305c8e5b0dcb011ea635?rik=2G7aTi9DvqYjZA&pid=ImgRaw&r=0"),
-                          _buildContainer("Technology", "https://th.bing.com/th/id/R.c59cb4d6dda0d97d72600fc66821bbcd?rik=p%2bqtqo2eNb0jtA&riu=http%3a%2f%2fthestatetimes.com%2fwp-content%2fuploads%2f2018%2f02%2finfotech.jpg&ehk=FF2w7obphoNFJAmHb0IyqQuqdiAVkB1haRtebXosqj8%3d&risl=&pid=ImgRaw&r=0"),
-                          _buildContainer("Health", "https://justiceaction.org.au/wp-content/uploads/2020/09/iStock-639896942.jpg"),
-                          _buildContainer("Science", "https://th.bing.com/th/id/R.f5b157c8033d028c69ff1a25f7d15133?rik=PaThKXiqrYYd5g&riu=http%3a%2f%2fthelibertarianrepublic.com%2fwp-content%2fuploads%2f2016%2f02%2fscience.jpg&ehk=fUOTF2D6E%2fQu6taI9nd58LHfbyA3naqv4va96Cv2mLg%3d&risl=&pid=ImgRaw&r=0"),
-                        ],
+                      child: ListView.builder(
+                        itemCount: childrenContainers.length,
+scrollDirection: Axis.horizontal,
+itemBuilder: (context, index) {
+  return GestureDetector(
+    child: _buildContainer(index),
+  );
+},
+                        
                       ),
                     ),
                     const SizedBox(height: 20),
@@ -125,7 +126,11 @@ class _HomePageState extends State<HomePage> {
   }
 
 
-  Widget _buildContainer(String title, String imageUrl) {
+  Widget _buildContainer(int index) {
+    String title = childrenContainers[index]["title"]!;
+    String imageUrl = childrenContainers[index]["imageURL"]!;
+
+
     
     return Container(
       margin: const EdgeInsets.all(8.0),
